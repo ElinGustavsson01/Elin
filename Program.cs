3f0db99a4da9bb794e678969ae2e061410fd0c51
ElinGustavsson <Elin.Gustavsson@iths.se>
2026-09-11 09:40:01 +0200

    Changed name

--- Program.cs
+++ Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git!");
+        Console.WriteLine(DoSomething("Elin"));
+    }
+    static string DoSomething(string name)
+    {
+        return $"Hello {name}";
     }
 }
